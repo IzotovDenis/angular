604318b72c17221d68fb780e2c9242f4e0358d94
den <den@ubuntu.(none)>
2015-03-13 09:56:39 +1000

    fix russian chars

--- app/helpers/find_helper.rb
+++ app/helpers/find_helper.rb
@@ -1,8 +1,9 @@
+# coding: utf-8
 module FindHelper
 
 	def str_query(query)
 		string = query.dup
-		string.gsub!(/[^0-9A-Za-z]/, ' ')
+		string.gsub!(/[^0-9A-Za-zА-Яа-я]/, ' ')
 		string.gsub!(/(\d)/, ' \1 ')
 		d = []
 		d << ["(#{string} | *#{string}*)"]
