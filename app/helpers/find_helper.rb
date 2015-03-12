299fc535811b0e5be0dc0fbe989d74352242c1be
den <den@ubuntu.(none)>
2015-03-13 09:40:56 +1000

    fix find

--- app/helpers/find_helper.rb
+++ app/helpers/find_helper.rb
@@ -3,6 +3,7 @@ module FindHelper
 	def str_query(query)
 		string = query.dup
 		string.gsub!(/[^0-9A-Za-z]/, ' ')
+		string.gsub!(/(\d)/, ' \1')
 		d = []
 		d << ["(#{string} | *#{string}*)"]
 		string.split(" ").each do |i|
