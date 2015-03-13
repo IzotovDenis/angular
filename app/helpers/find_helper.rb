dda4ecd7fc65d1aa5358fac8dd6052aaa7447e7a
den <den@ubuntu.(none)>
2015-03-13 10:50:23 +1000

    fix find

--- app/helpers/find_helper.rb
+++ app/helpers/find_helper.rb
@@ -2,19 +2,32 @@
 module FindHelper
 
 	def str_query(query)
+		#создаем копию запроса
 		string = query.dup
+		#Оставляем только разрешенные символы
 		string.gsub!(/[^0-9A-Za-zА-Яа-я]/, ' ')
+		# Формируем строку для поиска
+		d = string_find(string)
+		#Если в строке цифра добавляем разделяем ее пробелами
 		string.gsub!(/(\d)/, ' \1 ')
+		d << " | #{string_find(string)}"
+		string.delete!(" ")
+		d << " | (#{string} | *#{string}*)"
+	end
+
+	def string_find(string)
 		d = []
 		d << ["(#{string} | *#{string}*)"]
 		string.split(" ").each do |i|
 			d << ["(#{i} | *#{i}*)"]
 		end
-		string.delete!(" ")
 		d = d.join(" & ")
 		d = "( #{d} )"
-		d << " | (#{string} | *#{string}*)"
-
+		return d
 	end
 
+
+	def te
+		string.gsub!(/(\d)/, ' \1 ')
+	end
 end
